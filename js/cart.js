c996bc3166a76d5faff14105a25aed081b70dc42
nathalieutstumo <nathalie.utstumo@gmail.com>
2021-12-18 01:13:48 +0100

    update cart

--- js/cart.js
+++ js/cart.js
@@ -1,16 +1,58 @@
-if (localStorage.getItem('cart') !== null) {
+// if (localStorage.getItem('cart') !== null) {
+//     var total = 0;
+//     var items = JSON.parse(localStorage.getItem('cart'));
+  
+//     items.forEach(function(element) {
+//       document.querySelector('.cart__content').innerHTML +=
+//       '<li>' +
+//         element.title + ' ' + element.price + 'NOK' + ' Quantity: ' + element.quantity
+//       '</li>';
+//       total = total + (element.price * element.quantity)
+//     })
+  
+//     document.querySelector('.cart__total').innerHTML = 'TOTAL :' + total + 'NOK';
+  
+//   }
+  
+  var cart_items = [];
+
+if (localStorage.getItem('cart') === null) {
+
+  document.querySelector('.cart__content').innerHTML = 'Cart is empty';
+
+} else {
+
+  var total = 0;
+
+  cart_items = JSON.parse(localStorage.getItem('cart'));
+
+  cart_items.forEach(function(element) {
+    document.querySelector('.cart__content').innerHTML +=
+    '<li>' +
+      element.title + ' - ' + element.price + ' NOK - Quantity: ' + element.quantity + ' <button class="delete" data-id="'+element.id+'">X</button> '
+    '</li>';
+    total = total + (element.price * element.quantity);
+  })
+
+  document.querySelector('.cart__total').innerHTML = 'TOTAL : ' + total + 'NOK';
+
+}
+
+document.addEventListener('click',function(e){
+
+  if(e.target.className === 'delete') {
     var total = 0;
-    var items = JSON.parse(localStorage.getItem('cart'));
-  
-    items.forEach(function(element) {
-      document.querySelector('.cart__content').innerHTML +=
-      '<li>' +
-        element.title + ' ' + element.price + 'NOK' + ' Quantity: ' + element.quantity
-      '</li>';
-      total = total + (element.price * element.quantity)
-    })
-  
-    document.querySelector('.cart__total').innerHTML = 'TOTAL :' + total + 'NOK';
-  
+    var index = cart_items.findIndex( (item) => item.id === parseInt(e.target.dataset.id));
+    if (index >= 0) { // Delete if index found
+      cart_items.splice(index, 1);
+      var myobj = e.target.parentElement;
+      myobj.remove();
+      localStorage.setItem('cart', JSON.stringify(cart_items));
+      cart_items.forEach(function(element) {
+        total = total + (element.price * element.quantity);
+      })
+      document.querySelector('.cart__total').innerHTML = 'TOTAL : ' + total + 'NOK';
+    }
   }
-  
+
+});
